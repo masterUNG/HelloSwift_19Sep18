b68fc2c669e3fc3650d8d8ab7bf44b329b439a38
masterUNG <phrombutr@gmail.com>
2018-09-19 13:27:18 +0700

    get Event From Click Success

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    Explicit
+    let nameString: String = "Doramon"
+    
+    
+    
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    
+    
+    
+    
+    @IBAction func changeLabelButton(_ sender: Any) {
+        
+        print("You Click Button")
+        helloLabel.text = nameString
+        
+    }   // changeLabel
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   // Main Method
 
 
-}
+}   // Main Class
 
